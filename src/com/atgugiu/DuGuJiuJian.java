86a037120e3fb156ff5063ee3e8b8161c2120002
yuanjing <yuanvjing@126.com>
2018-08-30 10:11:37 +0800

    更新操作，来自王五

--- src/com/atgugiu/DuGuJiuJian.java
+++ src/com/atgugiu/DuGuJiuJian.java
@@ -5,5 +5,17 @@ public class DuGuJiuJian {
 		System.out.println("独孤九剑，李四");
 		System.out.println("我发现我不会用eclipse的快捷键了");
 		System.out.println("GG了,独孤九剑，东方不败，令狐冲，岳不群");
+		System.out.println("独孤九剑，李四");
+		System.out.println("我发现我不会用eclipse的快捷键了");
+		System.out.println("GG了,独孤九剑，东方不败，令狐冲，岳不群");
+		System.out.println("独孤九剑，李四");
+		System.out.println("我发现我不会用eclipse的快捷键了");
+		System.out.println("GG了,独孤九剑，东方不败，令狐冲，岳不群");
+		System.out.println("独孤九剑，李四");
+		System.out.println("我发现我不会用eclipse的快捷键了");
+		System.out.println("GG了,独孤九剑，东方不败，令狐冲，岳不群");
+		System.out.println("独孤九剑，李四");
+		System.out.println("我发现我不会用eclipse的快捷键了");
+		System.out.println("GG了,独孤九剑，东方不败，令狐冲，岳不群");
 	}
 }
