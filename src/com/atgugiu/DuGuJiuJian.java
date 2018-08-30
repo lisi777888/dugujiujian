217276ecb1a314c942c34e98b5a3c9cc6f752fef
yuanjing <Administrator@jingyuan>
2018-08-30 10:21:49 +0800

    来自李四的更新操作

--- src/com/atgugiu/DuGuJiuJian.java
+++ src/com/atgugiu/DuGuJiuJian.java
@@ -5,5 +5,7 @@ public class DuGuJiuJian {
 		System.out.println("独孤九剑，李四");
 		System.out.println("我发现我不会用eclipse的快捷键了");
 		System.out.println("GG了,独孤九剑，东方不败，令狐冲，岳不群");
+		System.out.println("我不知道你更改了代码，我也要更改提交 ");
+
 	}
 }
